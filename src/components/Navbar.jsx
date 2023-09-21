6f04213274f81f59ab2e8977306696558955d1df
cecidlp <delaparra.cecilia@gmail.com>
2023-09-21 22:24:47 +0200

    changed padding and transition from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             <div onClick={handleNav} className="block md:hidden">
                 {nav ? <AiOutlineClose fill="#E5E2DB" size={20} /> : <AiOutlineMenu fill="#E5E2DB" size={20} />}
             </div>
-            <ul className={nav ? "fixed left-0 top-0 pt-10 w-[100%] border-r rounded-b-[30px] border-r-gray-900 bg-[#5B726C] ease-in-out duration-500 z-20" : "ease-in-out duration-500 fixed top-[-100%] "}>
+            <ul className={nav ? "shadow-xl fixed left-0 top-0 pt-2 w-[100%] border-r rounded-b-[30px] border-none bg-[#5B726C] ease-in-out duration-500 z-20 " : "fixed top-[-100%] transition-top ease-in-out duration-500 z-20"}>
                 <div onClick={handleNav} className="flex items-center px-6">
                     <h1 className="w-full text-2xl  py-6 font-light text-[#E5E2DB]">Troot</h1>
                     <AiOutlineClose fill="#E5E2DB" size={20} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      transitionProperty:{
+        'top': 'top',
+      },
       borderColor: {
         "secondary": "#5B726C",
       },
