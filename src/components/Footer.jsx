0fb82bb39a74edcb6470b2ad205a70c0123d79c6
cecidlp <delaparra.cecilia@gmail.com>
2023-09-21 23:02:33 +0200

    changed phone number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className='mx-6 mt-16 py-10 md:px-5 lg:px-20 xl:px-32 border-t border-gray-400'>
         <h3 className='text-[#3C3B39] font-[600] text-[1.2rem] md:text-[1.5rem] lg:text-[1.8rem] pb-5'>TROOT</h3>
         <p className='text-[#3C3B39] text-[0.7rem] md:text-[1rem] lg:text-[1.1rem] xl:text-[1.3rem] pb-3'>trootranslations@gmail.com</p>
-        <p className='text-[#3C3B39] text-[0.7rem] md:text-[1rem] lg:text-[1.1rem] xl:text-[1.3rem] pb-3'>11 5117 9572 / 11 6529 1959</p>
+        <p className='text-[#3C3B39] text-[0.7rem] md:text-[1rem] lg:text-[1.1rem] xl:text-[1.3rem] pb-3'>+54 9 11 5117 9572 / +54 9 11 6529 1959</p>
     </div>
   )
 }
